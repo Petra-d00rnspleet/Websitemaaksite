06e83127769ee59bdcef91240917b75f8892bbe8
Petra-d00rnspleet <58157351@leerling.agoramaasenpeel.nl>
2026-09-07 18:35:37 +0200

    Delete firebase-init.js

--- firebase-init.js
+++
@@ -1,7 +0,0 @@
-// Initialiseert de Firebase-app en maakt de Realtime Database
-// beschikbaar als `window.db` voor de andere scripts.
-// Vereist dat firebase-config.js en de Firebase SDK-scripts
-// vóór dit bestand zijn geladen (zie de <script>-volgorde in de HTML).
-
-firebase.initializeApp(firebaseConfig);
-window.db = firebase.database();
